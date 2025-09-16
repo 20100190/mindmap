30337301cdd55089738403f00dc77bda4bed8ac3
Usman174 <usman.afiniti174@gmail.com>
2025-09-17 04:24:37 +0500

    adding async support for sql5

--- db/session.py
+++ db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 import asyncio
 from sqlalchemy.orm import declarative_base
-from google.cloud.sql.connector import Connector, IPTypes, ConnectorAsync
+from google.cloud.sql.connector import Connector, IPType
 import os
 
 
